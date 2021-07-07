072d52cc4e73289bf3ac96fc08160168fd2968f7
eyupbyr <eyupbyr@gmail.com>
2021-07-07 16:46:03 +0300

    Odev-1.3 eklendi

---
+++ C# 101/Odev-1/3/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). Sonrasında kullanıcıdan n adet kelime girmesi isteyin. Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
+*/
+using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number!");
+            int n = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter " + n + " word(s)!");
+            String[] words = new String[n];
+
+            for(int i = 0; i < n; i++)
+                words[i] = Console.ReadLine();
+
+            Console.WriteLine("Reverse order:");
+            for(int i = n-1; i >= 0; i--)
+                Console.WriteLine(words[i]);
+
+            Console.ReadKey();
+        }
+    }
+}
