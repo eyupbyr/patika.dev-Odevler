4161e8f3686154803698350b4af09443235d5028
eyupbyr <eyupbyr@gmail.com>
2021-07-07 16:56:02 +0300

    Odev-1.4 Eklendi

---
+++ C# 101/Odev-1/4/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. Cümledeki toplam kelime ve harf sayısını console'a yazdırın.*/
+using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence");
+            String input = Console.ReadLine();
+
+            String[] words;
+            words = input.Split(" ");
+            Console.WriteLine(words.Length);
+
+            int letters = 0;;
+            foreach(String s in words)
+                letters += s.Length;
+            Console.WriteLine(letters);
+            
+            Console.ReadKey();
+        }
+    }
+}
